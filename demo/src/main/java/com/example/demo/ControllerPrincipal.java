f8167321c19c9beafe3dca02c78c15364f169bbc
David-Rubio-Caballlero <d.rubio.2019@alumnos.urjc.es>
2024-02-13 18:24:24 +0100

    conectado registro e inicio sesion con controller

--- demo/src/main/java/com/example/demo/ControllerPrincipal.java
+++ demo/src/main/java/com/example/demo/ControllerPrincipal.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControllerPrincipal {
- @GetMapping("/")
- public String greeting(Model model) {
-//  model.addAttribute("name", "World");
- return "Reg";
- }
+    @GetMapping("/")
+    public String moveToReg(Model model) {
+    //  model.addAttribute("ses", "sesión");
+    return "Reg";
+    }
+
+    @GetMapping("/IniSes")
+    public String moveToIniSes(Model model) {
+    //  model.addAttribute("ses", "sesión");
+    return "IniSes";
+    }
 }
