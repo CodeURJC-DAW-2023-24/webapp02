5221499fa05f16c02269bc8af6b3dc2e771774bb
Javier-Vallejo <j.vallejo.2019@alumnos.urjc.es>
2024-02-21 20:37:30 +0100

    cambios para que se conecte con la base de datos bien
    (el problema era que tuviera una "_" en los valores)

--- C&Read/src/main/java/com/example/candread/Controller/Model/User.java
+++ C&Read/src/main/java/com/example/candread/Controller/Model/User.java
@@ -4,8 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
-
+@Entity
 public class User {
+   
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
