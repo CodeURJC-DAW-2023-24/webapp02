47a375ef880310a7278054e41bca6a6aa13f43d2
David-Rubio-Caballlero <d.rubio.2019@alumnos.urjc.es>
2024-02-21 23:09:19 +0100

    IniciarSesion con datos de la base de datos

--- C&Read/src/main/java/com/example/candread/Controller/Controller/UserController.java
+++ C&Read/src/main/java/com/example/candread/Controller/Controller/UserController.java
@@ -3,8 +3,8 @@ package com.example.candread.Controller.Controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -38,6 +38,23 @@ public class UserController {
             ModelAndView modelAndView = new ModelAndView("error-page");
             // Puedes agregar objetos al modelo si es necesario
             modelAndView.addObject("error", "Error al agregar usuario");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String name, @RequestParam String password1, Model model) {
+        Optional<User> userOptional = userRepository.findByNameAndPassword1(name, password1);
+
+        if (userOptional.isPresent()) {
+            // Usuario y contraseña válidos, redirige a la página principal
+            ModelAndView modelAndView = new ModelAndView("W-Main");
+            modelAndView.addObject("message", "Inicio de sesión exitoso");
+            return modelAndView;
+        } else {
+            // Usuario o contraseña incorrectos, redirige a la página de inicio de sesión con un mensaje de error
+            ModelAndView modelAndView = new ModelAndView("W-LogIn");
+            modelAndView.addObject("error", "Usuario o contraseña incorrectos");
             return modelAndView;
         }
     }
