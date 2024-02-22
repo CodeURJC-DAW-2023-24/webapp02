04c385e0e1062ad37b6cd1e53c875eeefbfd4024
Aurora-Fernandez <am.fernandez.2019@alumnos.urjc.es>
2024-02-23 00:25:24 +0100

    general operation to pass the user to other pages

--- C&Read/src/main/java/com/example/candread/Controller/ControllerPrincipal.java
+++ C&Read/src/main/java/com/example/candread/Controller/ControllerPrincipal.java
@@ -15,26 +15,24 @@ public class ControllerPrincipal {
     //Moverse al main, es la pagina principal y la primera que sale al entrar
     @GetMapping("/")
     public String moveToMain(Model model, HttpSession session) {
-        String username = null;
-        User user = (User) session.getAttribute("user");
-        if (user!=null) {
-            username = user.getName();
-        }
+        String username = getUserName(session);
         model.addAttribute("username", username);
     return "W-Main";
     }
 
     //Moverse a las bibliotecas
     @GetMapping("/Library")
-    public String moveToLibrary(Model model) {
-    //  model.addAttribute("ses", "sesión");
+    public String moveToLibrary(Model model, HttpSession session) {
+        String username = getUserName(session);
+        model.addAttribute("username", username);
     return "W-Library";
     }
 
     //moverse a un elemento de la biblioteca
     @GetMapping("/SingleElement")
-    public String moveToSingleScreen(Model model) {
-    //  model.addAttribute("ses", "sesión");
+    public String moveToSingleScreen(Model model, HttpSession session) {
+        String username = getUserName(session);
+        model.addAttribute("username", username);
     return "W-SingleElement";
     }
 
@@ -54,15 +52,26 @@ public class ControllerPrincipal {
 
     //moverse al perfil
     @GetMapping("/Profile")
-    public String moveToPerfil(Model model) {
-    //  model.addAttribute("ses", "sesión");
+    public String moveToPerfil(Model model, HttpSession session) {
+        String username = getUserName(session);
+        model.addAttribute("username", username);
     return "W-Profile";
     }
 
     //moverse a la pantalla de administrador
     @GetMapping("/Admin")
-    public String moveToAdmin(Model model) {
-    //  model.addAttribute("ses", "sesión");
+    public String moveToAdmin(Model model, HttpSession session) {
+        String username = getUserName(session);
+        model.addAttribute("username", username);
     return "W-Admin";
+    }
+
+    private String getUserName(HttpSession session){
+        String username = null;
+        User user = (User) session.getAttribute("user");
+        if (user!=null) {
+            username = user.getName();
+        }
+        return username;
     }
 }
