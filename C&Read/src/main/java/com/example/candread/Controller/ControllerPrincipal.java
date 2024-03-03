c1ab2fa25c67abdefbcc1c3b6d2db0de86c0e55f
Aurora-Fernandez <am.fernandez.2019@alumnos.urjc.es>
2024-03-03 23:37:27 +0100

    merge correctly

--- C&Read/src/main/java/com/example/candread/Controller/ControllerPrincipal.java
+++ C&Read/src/main/java/com/example/candread/Controller/ControllerPrincipal.java
@@ -26,19 +26,15 @@ import com.example.candread.repositories.ElementRepository;
 import com.example.candread.repositories.NewRepository;
 import com.example.candread.repositories.PagingRepository;
 import com.example.candread.services.ElementService;
-<<<<<<< HEAD
 import com.example.candread.services.UserService;
-=======
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Text;
->>>>>>> 4f5faffa14fad2d2002ff73e1ae1e9ffd06c67bf
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-
 
 @Controller
 public class ControllerPrincipal {
@@ -93,50 +89,32 @@ public class ControllerPrincipal {
 
     @GetMapping("/downloadNames")
     @ResponseBody
-    public ResponseEntity<byte[]> downloadNames(Model model, HttpSession session, Pageable pageable) throws IOException {
-        List<String> names = obtenerNombresDeLibros(model, pageable); 
-
-<<<<<<< HEAD
+    public ResponseEntity<byte[]> downloadNames(Model model, HttpSession session, Pageable pageable)
+            throws IOException {
+        List<String> names = obtenerNombresDeLibros(model, pageable);
         // Convert the list of names to a string separated by line breaks
-         String content = String.join("\n", names);
-=======
         byte[] pdfBytes = generatePdf(names);
-
-        // Convierte la lista de nombres a una cadena separada por saltos de línea
-        String content = String.join("\n", names);
->>>>>>> 4f5faffa14fad2d2002ff73e1ae1e9ffd06c67bf
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
         headers.setContentDispositionFormData("attachment", "nombres_de_libros.pdf");
 
-    return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+        return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
     }
 
     private List<String> obtenerNombresDeLibros(Model model, Pageable pageable) {
         // Logic to get the names of the books
-        
+
         User user = (User) model.getAttribute("user");
-<<<<<<< HEAD
-        if (user!=null){
-            Long userid =user.getId();
-            Page<Element> userBooks = pagingRepository.findByUsersIdAndType(userid, "LIBRO", pageable);
-    
-            List<String> names = new ArrayList<>();
-    
-            for (Element book : userBooks.getContent()) {
-=======
-        Long userid =user.getId();
-        //Page<Review> books= reviewRepository.findByUserLinked(userid, pageable);
-        Page<Element> userBooks = pagingRepository.findByUsersIdAndType(userid, "LIBRO", pageable);
-
         List<String> names = new ArrayList<>();
-
-        for (Element book : userBooks.getContent()) {
-            names.add(book.getName()); // Reemplaza "getNombre()" con el método real para obtener el nombre del libro
+        if (user != null) {
+            Long userid = user.getId();
+            Page<Element> userBooks = pagingRepository.findByUsersIdAndType(userid, "LIBRO", pageable);
+            for (Element book : userBooks.getContent()) {
+                names.add(book.getName());
+            }
+            
         }
-        
-        // Añade más nombres según tu lógica
         return names;
     }
 
@@ -156,120 +134,10 @@ public class ControllerPrincipal {
         return pdfOutputStream.toByteArray();
     }
 
-    // moverse al perfil
-    @GetMapping("/Profile")
-    public String moveToPerfil(Model model, HttpSession session, @RequestParam("page") Optional<Integer> page, Pageable pageable) throws SQLException, IOException {
-        int pageNumber = page.orElse(0);
-        int pageSize = 10;
-        pageable = PageRequest.of(pageNumber, pageSize);
-        int numberBooks = 0;
-        int numberFilms = 0;
-        int numberSeries = 0;
-
-        //NUMBER OF GENRES
-        int numberAction = 0;
-        int numberTerror = 0;
-        int numberAventura = 0;
-        int numberMisterio = 0;
-        int numberRomance = 0;
-        int numberCiencia = 0;
-        int numberDrama = 0;
-        int numberInfantil = 0;
-        int numberComedia = 0;
-        int numberFantasia = 0;
-        int numberSobrenatural = 0;
-        int numberNovela = 0;
-        int numberJuvenil = 0;
-
-        String typeNow;
-        List<String> genresNow = new ArrayList<>();
-        int limit = 0;
-
-        elementService.fullSet64Image();
-
-        //I extract the actual User from the model to use his Id
-        User user = (User) model.getAttribute("user");
-
-        //FOR AND SWITCH CASE TO GET ALL NUMBER OF MEDIA REGISTERED IN THE USER
-        limit = user.getElements().size();
-        for(int i = 0; i<limit; i = i+1){
-            typeNow = user.getElements().get(i).getType();
-            switch (typeNow){
-                case "LIBRO":
-                    numberBooks = numberBooks +1;
-                    break;
-                case "SERIE":
-                    numberSeries = numberSeries +1;
-                    break;
-                case "PELICULA":
-                    numberFilms = numberFilms + 1;
-                    break;
-
-                default:
-                    break;
-            } 
-        }
-        String genreNow;
-        for(int i = 0; i<limit; i = i+1){
-            genresNow = user.getElements().get(i).getGeneros();
-            int limitNow = genresNow.size();
-            for(int j = 0; j<limitNow; j = j+1){
-                genreNow = genresNow.get(j);
-                switch (genreNow) {
-                    case "ACCION":
-                        numberAction = numberAction +1;
-                        break;
-                    case "TERROR":
-                        numberTerror = numberTerror +1;
-                        break;
-                    case "AVENTURA":
-                        numberAventura = numberAventura + 1;
-                        break;
-                    case "MISTERIO":
-                        numberMisterio = numberMisterio +1;
-                        break;
-                    case "ROMANCE":
-                        numberRomance = numberRomance +1;
-                        break;
-                    case "CIENCIAFICCION":
-                        numberCiencia = numberCiencia + 1;
-                        break; 
-                    case "DRAMA":
-                        numberDrama = numberDrama +1;
-                        break;
-                    case "INFANTIL":
-                        numberInfantil = numberInfantil +1;
-                        break;
-                    case "COMEDIA":
-                        numberComedia = numberComedia + 1;
-                        break; 
-                    case "FANTASIA":
-                        numberFantasia = numberFantasia +1;
-                        break;
-                    case "SOBRENATURAL":
-                        numberSobrenatural = numberSobrenatural +1;
-                        break;
-                    case "NOVELA":
-                        numberNovela = numberNovela + 1;
-                        break; 
-                    case "JUVENIL":
-                        numberJuvenil = numberJuvenil + 1;
-                        break; 
-                    default:
-                        break;
-                }
->>>>>>> 4f5faffa14fad2d2002ff73e1ae1e9ffd06c67bf
-            }
-            return names;
-        }else{
-            return null;
-        }
-        
-    }
-    
     @GetMapping("/Admin")
     public String moveToAdmin(Model model) throws SQLException, IOException {
-        userService.fullSet64Image();;
+        userService.fullSet64Image();
+        ;
         return "W-Admin";
     }
 
@@ -282,7 +150,5 @@ public class ControllerPrincipal {
     public String moveToErrorLoginError(Model model) {
         return "W-Error";
     }
-
-    
 
 }
