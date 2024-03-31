7655c687134aa3f94eb8f3f65101d5be6612751f
Javier-Vallejo <j.vallejo.2019@alumnos.urjc.es>
2024-03-31 19:01:33 +0200

    fix merged

--- C&Read/src/main/java/com/example/candread/apicontroller/UserApiController.java
+++ C&Read/src/main/java/com/example/candread/apicontroller/UserApiController.java
@@ -1,5 +1,3 @@
-
-
 package com.example.candread.apicontroller;
 
 import java.net.URI;
@@ -7,22 +5,9 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.SQLException;
-import java.util.Base64;
-import java.util.List;
-import java.util.Optional;
-
-import javax.sql.rowset.serial.SerialBlob;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,46 +15,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.candread.dto.UserDTO;
-import com.example.candread.dto.UserDTO;
-import com.example.candread.model.Element;
 import com.example.candread.model.User;
 import com.example.candread.services.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("api/user")
 public class UserApiController {
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @Autowired
     private UserService userService;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
     
-    private UserRepository userRep;
-
     @GetMapping("/")
     public Page<User> getUsers(Pageable pageable) {
-        return userRep.findAll(pageable);
+        return userService.repoFindAll(pageable);
     }
 
     @PostMapping("/add")
@@ -77,20 +44,17 @@ public class UserApiController {
             HttpServletRequest request) {
         try {
             // se crea un usuario y se le asigna uno o varios roles
-
             User userPrueba = new User(userDTO.getName(), passwordEncoder.encode(userDTO.getPassword()), "USER");
-
-            userRep.save(userPrueba);
+            //We save the user in the DDBB
+            userService.repoSaveUser(userPrueba);
             Long userId = userPrueba.getId();
             String userURL = ServletUriComponentsBuilder.fromRequestUri(request).path("/{id}").buildAndExpand(userId)
                     .toUriString();
             return ResponseEntity.created(new URI(userURL)).build();
-
         } catch (Exception e) {
             System.out.print("error:");
         }
         return null;
-
     }
 
     @PutMapping("/edit/{id}")
@@ -98,8 +62,8 @@ public class UserApiController {
             @RequestBody UserDTO userDTO,
             HttpServletRequest request) throws URISyntaxException {
 
-        Optional<User> optUser = userRep.findById(id);
-
+        Optional<User> optUser = userService.repoFindById(id);
+        
         if (optUser.isPresent()) {
             User user = (User) optUser.get();
             if (userDTO.getName() != null) {
@@ -118,49 +82,11 @@ public class UserApiController {
                 }
                 user.setRoles(rolesList);
             }
-
-            userRep.save(user);
+            //We save the user in the DDBB
+            userService.repoSaveUser(user);
             return ResponseEntity.ok(user);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
-
-    @PostMapping("/edit/{id}/image")
-    public ResponseEntity<Object> uploadUserImageById(@PathVariable Long id,
-            @RequestParam("perfilImage") MultipartFile perfilImage,
-            @RequestParam("bannerImage") MultipartFile bannerImage,
-             HttpServletRequest request)
-            throws URISyntaxException, IOException {
-        Optional<User> optUser = userRep.findById(id);
-
-        if (optUser.isPresent()) {
-            User user = (User) optUser.get();
-            try {
-                // Set the image to the element
-                byte[] imageData = perfilImage.getBytes();
-                user.setProfileImage(new SerialBlob(imageData));
-                byte[] imageDataBanner = bannerImage.getBytes();
-                user.setBannerImage(new SerialBlob(imageDataBanner));
-
-                user.setBase64BannerImage((Base64.getEncoder().encodeToString(imageDataBanner)));
-                user.setBase64ProfileImage((Base64.getEncoder().encodeToString(imageData)));
-                // Save in the database
-                userRep.save(user);
-
-                String imageUrl = ServletUriComponentsBuilder.fromRequestUri(request).path("/{id}/image")
-                        .buildAndExpand(id).toUriString();
-
-                return ResponseEntity.created(new URI(imageUrl)).build();
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-            }
-        } else {
-            // Element not fount and we return 404 error
-            return ResponseEntity.notFound().build();
-        }
-    }
-
 }
