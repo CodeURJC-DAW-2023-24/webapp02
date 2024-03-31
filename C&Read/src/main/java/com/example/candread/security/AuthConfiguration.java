9fa15db40747e32057a34c61619cd7f78917d621
Javier-Vallejo <j.vallejo.2019@alumnos.urjc.es>
2024-03-31 16:59:09 +0200

    maybe final testing

--- C&Read/src/main/java/com/example/candread/security/AuthConfiguration.java
+++ C&Read/src/main/java/com/example/candread/security/AuthConfiguration.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@Configuration
 public class AuthConfiguration {
 
 
--- C&Read/src/main/java/com/example/candread/security/SecurityConfiguration.java
+++ C&Read/src/main/java/com/example/candread/security/SecurityConfiguration.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
@@ -23,6 +23,7 @@ import com.example.candread.repositories.UserRepository;
 
 @Configuration
 @EnableWebSecurity
+@Import(AuthConfiguration.class)
 public class SecurityConfiguration {
 
     @Autowired
