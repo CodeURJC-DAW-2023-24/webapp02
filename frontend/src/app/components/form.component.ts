c818c7447786a05ad313e0ef395bc8622f3dd3f7
Javier-Vallejo <j.vallejo.2019@alumnos.urjc.es>
2024-04-28 13:10:03 +0200

    set default casi hecho :)

--- frontend/src/app/components/form.component.ts
+++ frontend/src/app/components/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Element } from '../models/element.model';
 import { Observable } from 'rxjs';
@@ -10,11 +10,13 @@ import { Observable } from 'rxjs';
     templateUrl: '../Htmls/W-Form.component.html',
     styleUrl: '../Css/S-Header.css'
 })
-export  class FormComponent  { 
+export  class FormComponent implements OnChanges { 
     
-    // observer! : Observable<Element>
 
     @Input()
+    observer! : Observable<Element>
+
+    
     element!: Element;
     
 
@@ -34,30 +36,53 @@ export  class FormComponent  {
      constructor()  {
         this.init()
     }
+   
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['observer'] && this.observer) {
+
+
+            this.observer.subscribe((response: any) => {
+                this.element = response;
+                this.name = this.element.name
+                this.description = this.element.description
+                this.author = this.element.name
+                this.type= this.element.type
+                this.state = this.element.state
+                this.season = this.element.season
+                this.country = this.element.country
+                this.year = this.element.year
+          
+                })
+
+        }
+
+    }
 
     public async  init () {
 
+       
+
         // this.observer.subscribe((response: any) => {
         //     this.element = response;
-            // this.name = this.element.name
-            // this.description = this.element.description
-            // this.author = this.element.name
-            // this.type= this.element.type
-            // this.state = this.element.state
-            // this.season = this.element.season
-            // this.country = this.element.country
-            // this.year = this.element.year
+        //     this.name = this.element.name
+        //     this.description = this.element.description
+        //     this.author = this.element.name
+        //     this.type= this.element.type
+        //     this.state = this.element.state
+        //     this.season = this.element.season
+        //     this.country = this.element.country
+        //     this.year = this.element.year
       
         //     })
 
-            this.name = this.element.name
-            this.description = this.element.description
-            this.author = this.element.name
-            this.type= this.element.type
-            this.state = this.element.state
-            this.season = this.element.season
-            this.country = this.element.country
-            this.year = this.element.year
+            // this.name = await this.element.name
+            // this.description = await this.element.description
+            // this.author = await this.element.name
+            // this.type= await this.element.type
+            // this.state = await this.element.state
+            // this.season = await this.element.season
+            // this.country =  await this.element.country
+            // this.year =  await this.element.year
     }
     // name = elemnt.name
 
--- frontend/src/app/components/modify.component.ts
+++ frontend/src/app/components/modify.component.ts
@@ -35,7 +35,7 @@ export class ModifyComponent {
   //   const resultsContainer = document.getElementById('modifyBookForm');
   // }
 
-  search(name: string, type: string): Observable<Element[]> {
+  search(name: string, type: string): Observable<Element> {
 
 
     if (type == "LIBRO") {
@@ -44,6 +44,7 @@ export class ModifyComponent {
       this.elementSearched = response;
         // console.log(this.elementSearched.id)
       this.found = this.elementSearched != null
+      return this.observableElement
   
   
         })
@@ -62,6 +63,7 @@ export class ModifyComponent {
       this.filmService.getFilmByName(name).subscribe((response: any) => {
       this.elementSearched = response;
       this.found = this.elementSearched != null
+      return this.observableElement
       })
     }
 
@@ -69,83 +71,14 @@ export class ModifyComponent {
       this.bookService.getBookByName(name).subscribe((response: any) => {
       this.elementSearched = response;
       this.found = this.elementSearched != null
+      return this.observableElement
       })
     }
 
+    return this.observableElement
 
-    const books$ = this.getArray(this.bookService.getBooks());
-    const series$ = this.getArray(this.seriesService.getSeries());
-    const films$ = this.getArray(this.filmService.getFilms());
-
-    return forkJoin([books$, series$, films$]).pipe(
-      map(([booksArray, seriesArray, filmsArray]) => {
-        const elementsArray: Element[] = [];
-        elementsArray.push(...booksArray, ...seriesArray, ...filmsArray);
-        return elementsArray.filter(element => element.name === name);
-      })
-    );
+    
   }
 
-  private searchElement(booksArray: Observable<Element[]>, elementsArray: Element[], name: string): Element[] {
-    booksArray.subscribe({
-      next: (elements) => {
-        elementsArray.concat(elements);
-        var size = elements.length;
-        if (size === 1) {
-          var element = elements[0]
-
-
-
-          // this.http.get('assets/W-EditFragment.html', { responseType: 'text' })
-          //   .subscribe({
-          //     next: (html: string) => {
-          //       const resultsContainer = document.getElementById('modifyBookForm');
-          //       if (resultsContainer) {
-          //         resultsContainer.innerHTML = html;
-          //       }
-          //     },
-          //     error: (err: any) => {
-          //       console.error('Error al cargar el contenido HTML:', err);
-          //     }
-          //   });
-        }
-        else if (size == 2) {
-          var types: string[] = []
-          for (let i = 0; i < 2; i++) {
-            types.push(elements[i].type)
-          }
-
-        }
-      }
-    });
-    return elementsArray;
-  }
-
-  private searchType(element: Element) {
-    if (element.type === "LIBRO") {
-      if (element.id) {
-        var elementFound = this.bookService.getBook(element.id);
-        elementFound.subscribe();
-      }
-    } else if (element.type === "SERIE") {
-      if (element.id) {
-        var elementFound = this.seriesService.getSerie(element.id);
-        elementFound.subscribe();
-      }
-    } else if (element.type === "PELICULA") {
-      if (element.id) {
-        var elementFound = this.filmService.getFilm(element.id);
-        elementFound.subscribe();
-      }
-    }
-  }
-
-  getArray(jsonElement: Observable<Element[]>): Observable<Element[]> {
-    return jsonElement.pipe(
-      map((jsonElement) => {
-        const element = Object.entries(jsonElement);
-        return element[0][1] as unknown as Element[];
-      })
-    );
-  }
+  
 }
